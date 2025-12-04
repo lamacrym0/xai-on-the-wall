bda4e93e7fbd7b83a92013fe5db4208940fc2471
lamacrym0 <lamastico2.0@gmail.com>
2025-12-04 11:16:34 +0100

    CIU explainer done

--- explainer/ciu.py
+++ explainer/ciu.py
@@ -1,9 +1,42 @@
 import ciu
 import numpy as np
+import numpy as np
+import pandas as pd
+import torch
 
-def get_explain_CIU(model, data,out_name):
-    pred_test_prices = model.predict(data["X_test"])
-    inst_ind = np.argmax(pred_test_prices) # Test set instance with highest price estimate, so we expect a "positive" explanation.
-    instance = data["X_test"].iloc[[inst_ind]]
-    CIU = ciu.CIU(model.predict, out_name, data=data["X_train"])
-    return CIU.explain(instance)
+def get_explainer_CIU(model, data,output_names, feature_names):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device).eval()   
+    def torch_predict(X):
+        if isinstance(X, (pd.DataFrame, pd.Series)):
+            X_np = X.values.astype(np.float32)
+        else:
+            X_np = np.asarray(X, dtype=np.float32)
+
+        if X_np.ndim == 1:
+            X_np = X_np.reshape(1, -1)
+
+        with torch.no_grad():
+            X_tensor = torch.from_numpy(X_np).to(device)
+            y_tensor = model(X_tensor)            
+
+        y_np = y_tensor.detach().cpu().numpy()    
+        return y_np
+    X_train_df = pd.DataFrame(data["X_train"], columns=feature_names)
+
+    dataset_df = X_train_df.copy()
+    dataset_df["target"] = data["y_train"]
+
+    CIU_model = ciu.CIU(
+        predictor=torch_predict,
+        out_names=output_names,
+        data=X_train_df     
+    )
+    
+    return CIU_model
+
+def get_ciu_instance(CIU_model, instance):
+    res = CIU_model.explain(
+        instance=instance,
+    )
+    return res
